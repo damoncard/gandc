b750ee1c4f12bf934af57371b137ca2befbbec43
Sirawat <cardtergt@hotmail.com>
2016-04-20 19:34:13 +0700

    Add tableCheck button

--- Actor.java
+++ Actor.java
@@ -7,6 +7,7 @@ public class Actor {
 
 	private static LogFace login;
 	private static MainFace main;
+	private static TableFace table;
 
 	public static void main (String [] args) {
 		login = new LogFace();
@@ -24,11 +25,39 @@ public class Actor {
 		main.setVisible(true);
 	}
 
+	private static void checkTable() {
+		table = new TableFace();
+		table.getButton().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				reserveTablePerformed(e);
+			}
+		})
+		table.setVisible(true)
+	}
+
 	private static void signInActionPerformed(ActionEvent e) {
 
 		// Call method in model
 		if (/*checkLogin(login.getForm())*/true) {
 			mainPart();
 		}
+	}
+
+	private static void reserveTablePerformed(ActionEvent e) {
+
+		// Call method in model
+		/*if(lblMessage.getText().equals(message[0] + tableId)) {
+			MainFrame.setTableIdAndTime(tableId, dateTimeReserve);
+			
+			DateFormat dateTimeFormat = new SimpleDateFormat("dd-MM-yyyy   HH:mm");
+			dateTimeReserve.add(java.util.Calendar.YEAR, -543);
+			String dateString = dateTimeFormat.format(dateTimeReserve.getTime());
+			
+			MainFrame.setLabelDateAndTime(String.valueOf(tableId), dateString);
+			
+			dispose();
+		} else {
+			javax.swing.JOptionPane.showMessageDialog(null, "You did not select the table");
+		}*/
 	}
 }
