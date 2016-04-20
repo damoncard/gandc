f300974f073c2269007323b49f2f03f4d33367ff
Default141 <phoenixbird14@gmail.com>
2016-04-20 19:28:43 +0700

    Add logIn class
    LogIn class is a class that will check username and password with
    database

---
+++ Functions/LogIn.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Functions;
+/**
+ *
+ * @author Default141
+ */
+public class LogIn {
+    public boolean loginCheck(String user,String pass){
+        
+    /*    if(user.equals("databaseUserName") && pass.equals("databaseUserName")){
+            return true;
+        }
+            return false;
+    */
+        return true;
+    }
+}
