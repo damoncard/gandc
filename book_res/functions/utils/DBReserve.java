5d607ad21c2f02f24476a6982ef7a4b1cd93eeca
eawarnong <57130500219@st.sit.kmutt.ac.th>
2016-05-14 18:21:46 +0700

    add addOrders() in DBReserve

--- book_res/functions/utils/DBReserve.java
+++ book_res/functions/utils/DBReserve.java
@@ -1,6 +1,8 @@
 
 package book_res.functions.utils;
 
+import java.util.HashMap;
+
 public class DBReserve extends ConnectDB{
     
     public DBReserve() {
@@ -8,11 +10,10 @@ public class DBReserve extends ConnectDB{
     }
 
     /**
-     * 
      * @param tableId
      * @param customerId
      * @param dateReserve in format of "YYYY-MM-DD"
-     * @param timeReserve in format of "HH:MM:SS"
+     * @param timeReserve in format of "HH:MM"
      */
     public void insertReserving(int tableId, int customerId, String dateReserve, String timeReserve) {
         String sql = "INSERT INTO OOSD_RESERVES(dateReserve, timeReserve, tableID, customerID) VALUES ( '"
@@ -20,24 +21,28 @@ public class DBReserve extends ConnectDB{
                 + timeReserve + "', "
                 + tableId + ", "
                 + customerId
-                + " ) ";
+                + " )";
         System.out.println(db.executeQuery(sql));
-        sql = "SELECT IDENT_CURRENT('OOSD_RESERVES')";
-        System.out.println(db.queryRow(sql));
     }
     
-    public void getLastestOrderID() {
-        //String sql = "SELECT"
+    public int getLastestOrderID() {
+        String sql = "SELECT MAX(reserveID) AS id FROM OOSD_RESERVES";
+        HashMap id = db.queryRow(sql);
+        return Integer.parseInt(String.valueOf(id.get("id")));
     }
     
-    public void updateOrderedFood(int foodId, int quantity) {
+    public void addOrders(int foodID, int qty) {
+        String sql = "INSERT INTO OOSD_ORDERS(reserveID, foodID, quantity) VALUES ( "
+                + getLastestOrderID() + ", "
+                + foodID + ", "
+                + qty + " )";
+        db.executeQuery(sql);
+        updateOrderedFood(foodID, qty);
+    }
+    
+    private void updateOrderedFood(int foodId, int quantity) {
         String sql = "UPDATE OOSD_FOODS SET ordered = ordered + " + quantity + " WHERE foodID = " + foodId;
         db.executeQuery(sql);
-    }
-    
-    public static void main(String[] args) {
-        DBReserve r = new DBReserve();
-        r.insertReserving(203, 2, "2016-05-05", "13:00:00");
     }
 
 }
