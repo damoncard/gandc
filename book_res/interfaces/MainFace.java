8471af68b8dfb069c2b6e972bec041daf3cf1bcc
Default141 <phoenixbird14@gmail.com>
2016-05-21 00:05:49 +0700

    Create FoodFucntion & delete unessary import

--- book_res/interfaces/MainFace.java
+++ book_res/interfaces/MainFace.java
@@ -2,7 +2,7 @@ package book_res.interfaces;
 
 import java.sql.Timestamp;
 import java.awt.Dimension;
-import java.awt.Rectangle;
+
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.MouseEvent;
@@ -14,7 +14,7 @@ import java.util.GregorianCalendar;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
-import javax.swing.ImageIcon;
+
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
@@ -2058,70 +2058,3 @@ public class MainFace extends SimpleFace {
 
 }
 
-/*
-	javax.swing.JLabel snackPop, foodPop;
-	private void addPopularFood() {
-
-		java.util.ArrayList<java.util.HashMap> snacks = queue.getPopularSnacks();
-		java.util.ArrayList<java.util.HashMap> foods = queue.getPopularFoods();
-		snackPop = new javax.swing.JLabel("=====Snacks Popular=====");
-		foodPop = new javax.swing.JLabel("=====Foods Popular=====");
-		
-		System.out.println(snackButtons.get(snacks.get(0).get("foodName")));
-		System.out.println(snackButtons.get(snacks.get(1).get("foodName")));
-		System.out.println(foodButtons.get(foods.get(0).get("foodName")));
-		System.out.println(foodButtons.get(foods.get(1).get("foodName")));
-		System.out.println(foodButtons.get(foods.get(2).get("foodName")));
-		System.out.println(foodButtons.get(foods.get(3).get("foodName")));
-		
-
-		GroupLayout pnlLayout = new GroupLayout(pnlMenuButton);
-		pnlMenuButton.setLayout(pnlLayout);
-		pnlLayout.setHorizontalGroup(
-				pnlLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(pnlLayout.createSequentialGroup()
-						.addContainerGap()
-						.addGroup(pnlLayout.createParallelGroup(Alignment.LEADING)
-								.addGroup(pnlLayout.createSequentialGroup()
-										.addComponent(snackPop, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addGroup(pnlLayout.createSequentialGroup()
-										.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addGap(18, 18, 18)
-										.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addGroup(pnlLayout.createSequentialGroup()
-										.addComponent(foodPop, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addGroup(pnlLayout.createSequentialGroup()
-										.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addGap(18, 18, 18)
-										.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-								.addGroup(pnlLayout.createSequentialGroup()
-										.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addGap(18, 18, 18)
-										.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-		pnlLayout.setVerticalGroup(
-				pnlLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(pnlLayout.createSequentialGroup()
-						.addContainerGap()
-						.addGroup(pnlLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(snackPop, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGap(20, 20, 20)
-						.addGroup(pnlLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGap(18, 18, 18)
-						.addGroup(pnlLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(foodPop, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGap(20, 20, 20)
-						.addGroup(pnlLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addGap(18, 18, 18)
-						.addGroup(pnlLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(btnWater, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-						.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-		);
-	}
-*/
