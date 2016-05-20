65826ba8ef0a2849215eb8cffb1482c98c0272d6
eawarnong <57130500219@st.sit.kmutt.ac.th>
2016-05-20 20:57:45 +0700

    Add actionListener of chair button

--- book_res/functions/TableFuction.java
+++ book_res/functions/TableFuction.java
@@ -17,6 +17,7 @@ import book_res.functions.utils.DBCheckTable;
 import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -26,6 +27,7 @@ import java.util.GregorianCalendar;
  * @author Default141
  */
 public class TableFuction {
+
     static DBCheckTable checkTable;
     static TableFace tableface;
     static String time;
@@ -33,77 +35,84 @@ public class TableFuction {
     static int tableId;
     static String message1 = "Avalible Table Number: ";
     static String message2 = "Not Avalible";
-    static JButton b;
-    public TableFuction(){
-         checkTable = new DBCheckTable();
-         checkTable();
+    static ArrayList<JButton> chairButtons;
+
+    public TableFuction() {
+        checkTable = new DBCheckTable();
+        tableface = new TableFace();
+        chairButtons = tableface.getChairButtons();
+
+        checkTable();
+        checkSeat();
     }
+
+    private void checkTable() {
+        (tableface.getAddButton()).addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //reserveTablePerformed(e);
+            }
+        });
+
+        tableface.setVisible(true);
+    }
+
+    private void checkSeat() {
+        chairButtons.get(0).addActionListener(new ChairButtonAction(Integer.parseInt(String.valueOf(chairButtons.get(0).getText()))));
+        chairButtons.get(1).addActionListener(new ChairButtonAction(Integer.parseInt(String.valueOf(chairButtons.get(1).getText()))));
+        chairButtons.get(2).addActionListener(new ChairButtonAction(Integer.parseInt(String.valueOf(chairButtons.get(2).getText()))));
+        chairButtons.get(3).addActionListener(new ChairButtonAction(Integer.parseInt(String.valueOf(chairButtons.get(3).getText()))));
+    }
+
+    private void checkPerformed(int seats) {
+        tableId = checkTable.checkAvailable(String.valueOf(tableface.getDate().getValue()), String.valueOf(tableface.getTime().getValue()), seats);
+
+        if (tableId != -1) {
+            tableface.getMessage().setText(message1 + tableId);
+            tableface.getMessage().setForeground(new Color(0, 204, 0));
+        } else {
+            tableface.getMessage().setText(message2);
+            tableface.getMessage().setForeground(new Color(255, 0, 0));
+            this.tableId = tableId;
+        }
+    }
+    
+    class ChairButtonAction implements ActionListener {
+        int seats;
+        
+        public ChairButtonAction(int seats) {
+            this.seats = seats;
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            checkPerformed(seats);
+        }
+    }
+
+    /*private static void reserveTablePerformed(ActionEvent e) {
+     DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+     time = String.valueOf(tableface.getTime().getValue());
+     date = dateFormat.format(tableface.getDate().getValue());
+     tableId = checkTable.checkAvailable("2016-05-04", "13:00", 4);
+     //Calendar cal = new GregorianCalendar();
+     //cal.set(Calendar.ERA, GregorianCalendar.AD);
+     //cal.setTime(date);
+     //date.add(java.util.Calendar.YEAR, -543);
+     // Call method in model
+     //System.out.println(time +"       "+date);
+     if(tableId != -1){
+     tableface.getMessage().setText(message1+ tableId);
+     //tableface.getMessage().setForeground(new Color(0, 204, 0));
+     }else{
+     tableface.getMessage().setText(message2); 
+     //tableface.getMessage().setForeground(new Color(255, 0, 0));
+     }
+     }*/
     public static void main(String[] args) {
         TableFuction table = new TableFuction();
         //table.checkTable();
     }
+
     
-    private void checkTable() {
-		tableface = new TableFace();
-		(tableface.getAddButton()).addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        //reserveTablePerformed(e);
-                    }
-		});
-                
-		tableface.setVisible(true);
-    }
-        private void checkSeat() {
-		b.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				checkPerformed(e);
-			}
-		});
-    }
-    
-    private void checkPerformed(ActionEvent e) {
-		b = (JButton) e.getSource();
-                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                time = String.valueOf(tableface.getTime().getValue());
-                date = dateFormat.format(tableface.getDate().getValue());
-                tableId = checkTable.checkAvailable("2016-05-04", "13:00", 4);
-                //Calendar cal = new GregorianCalendar();
-                //cal.set(Calendar.ERA, GregorianCalendar.AD);
-                //cal.setTime(date);
-                //date.add(java.util.Calendar.YEAR, -543);
-		// Call method in model
-                //System.out.println(time +"       "+date);
- 
-		tableId = checkTable.checkAvailable(String.valueOf(tableface.getDate().getValue()),String.valueOf(tableface.getTime().getValue()), 4);
-			
-		if(tableId != -1) {
-			tableface.getMessage().setText(message1 + tableId);
-			tableface.getMessage().setForeground(new Color(0, 204, 0));
-		} else {
-			tableface.getMessage().setText(message2);
-			tableface.getMessage().setForeground(new Color(255, 0, 0));
-			this.tableId = tableId;
-		}
-    }
-    
-    /*private static void reserveTablePerformed(ActionEvent e) {
-                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                time = String.valueOf(tableface.getTime().getValue());
-                date = dateFormat.format(tableface.getDate().getValue());
-                tableId = checkTable.checkAvailable("2016-05-04", "13:00", 4);
-                //Calendar cal = new GregorianCalendar();
-                //cal.set(Calendar.ERA, GregorianCalendar.AD);
-                //cal.setTime(date);
-                //date.add(java.util.Calendar.YEAR, -543);
-		// Call method in model
-                //System.out.println(time +"       "+date);
-		if(tableId != -1){
-			tableface.getMessage().setText(message1+ tableId);
-                        //tableface.getMessage().setForeground(new Color(0, 204, 0));
-		}else{
-                   tableface.getMessage().setText(message2); 
-                   //tableface.getMessage().setForeground(new Color(255, 0, 0));
-                }
-	}*/
 }
