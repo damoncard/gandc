86c419054ec9ceadf332f58cdc8ff0b6f6b8aace
eawarnong <57130500219@st.sit.kmutt.ac.th>
2016-05-21 00:38:48 +0700

    change parameter of methods

--- book_res/functions/utils/DBFood.java
+++ book_res/functions/utils/DBFood.java
@@ -5,21 +5,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DBFood extends ConnectDB{
-    
-    private int foodID;
-    
+
     public DBFood() {
         super();
     }
-    
-    public void setFoodID(int foodID) {
-        this.foodID = foodID;
-    }
-    
-    public int getFoodID() {
-        return foodID;
-    }
-    
+ 
     public ArrayList<HashMap> getAllFoodIDs() {
         String sql = "SELECT foodID FROM OOSD_FOODS WHERE foodID BETWEEN 201 AND 299";
         return db.queryRows(sql);
@@ -35,19 +25,19 @@ public class DBFood extends ConnectDB{
         return db.queryRows(sql);
     }
     
-    public String getFoodName() {
+    public String getFoodName(int foodID) {
         String sql = "SELECT foodName FROM OOSD_FOODS WHERE foodID = " + foodID;
         HashMap name = db.queryRow(sql);
         return String.valueOf(name.get("foodName"));
     }
     
-    public int getPrice() {
+    public int getPrice(int foodID) {
         String sql = "SELECT price FROM OOSD_FOODS WHERE foodID = " + foodID;
         HashMap price = db.queryRow(sql);
         return Integer.parseInt(String.valueOf(price.get("price")));
     }
     
-    public int getOrdered() {
+    public int getOrdered(int foodID) {
         String sql = "SELECT ordered FROM OOSD_FOODS WHERE foodID = " + foodID;
         HashMap ordered = db.queryRow(sql);
         return Integer.parseInt(String.valueOf(ordered.get("ordered")));
