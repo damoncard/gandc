139e978d86ec994abd5d6bb8051926edbf4805d8
eawarnong <57130500219@st.sit.kmutt.ac.th>
2016-05-22 13:10:39 +0700

    55555555555

--- book_res/functions/utils/DBBacklog.java
+++ book_res/functions/utils/DBBacklog.java
@@ -16,11 +16,15 @@ public class DBBacklog extends ConnectDB{
         String year = date.substring(0, 4);
         String month = date.substring(5, 7);
         if(month.charAt(0) == '0') month = String.valueOf(month.charAt(1));
-        String sql = "SELECT date, noOfCustomer, revenue, foodName"
-                + " FROM OOSD_BACKLOG, OOSD_FOODS"
-                + " WHERE YEAR(date) = '" + year + "' AND MONTH(date) = '" + month + "'"
-                + " AND (date, foodID) IN (SELECT date, foodID FROM OOSD_FOODBACKLOG AS A WHERE A.qty >= ALL(SELECT qty FROM OOSD_FOODBACKLOG AS B WHERE A.date = B.date))";
+        String sql = "SELECT OOSD_BACKLOG.date, noOfCustomer, revenue, foodName"
+                + " FROM OOSD_FOODBACKLOG JOIN OOSD_FOODS ON OOSD_FOODBACKLOG.foodID = OOSD_FOODS.foodID"
+                + " JOIN OOSD_BACKLOG ON OOSD_FOODBACKLOG.date = OOSD_BACKLOG.date"
+                + " WHERE YEAR(OOSD_BACKLOG.date) = '" + year + "' AND  OOSD_BACKLOG.date IN ("
+                + " SELECT date FROM OOSD_FOODBACKLOG AS a"
+                + " HAVING qty > ALL(SELECT qty FROM OOSD_FOODBACKLOG AS b WHERE a.date = b.date and a.foodID <> b.foodID) )";
+        System.out.println(sql);
         return db.queryRows(sql);
+        
     }
     
     public ArrayList<HashMap> getBacklogAMonth(String year) {
